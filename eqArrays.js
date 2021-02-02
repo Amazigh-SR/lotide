431f5562c9fbd573acb7f7464ac77214c857b88f
Saïd Redouane <s_red009@hotmail.com>
2021-02-02 23:37:52 +0000

    eslinted the file - it is now premium!

--- eqArrays.js
+++ eqArrays.js
@@ -8,12 +8,12 @@ const assertEqual = function(actual, expected) {
 
 
 const eqArrays = function(arr1, arr2) {
-  if(arr1.length !== arr2.length) {
+  if (arr1.length !== arr2.length) {
     return false;
   }
 
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -32,6 +32,6 @@ assertEqual(eqArrays([], []), true);
 assertEqual(eqArrays([1, 2, 3], [8, 2, 3]), false);
 //4 - An example that has different data types
 assertEqual(eqArrays([1, 2, 3], ["John", "Martin", 3]), false);
+assertEqual(eqArrays(["1", "2", "3"], [1, "2", "3"]), false);
 //5 - A case where both arrays are equal
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], [1, "2", "3"]), false);
