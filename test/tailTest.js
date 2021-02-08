150b130225858eb04b4460981159e9a5c9467572
Saïd Redouane <s_red009@hotmail.com>
2021-02-08 20:54:52 +0000

    implemented the test cases with mocha and chai - removed everything related to assertEqual

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,27 +1,30 @@
 //Requiring both AssertEqual and tail functions
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
 // --------------------------------------------------- //
-// Test Cases:
+describe("#tail", function() {
+  it("return length of 2 for ['hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("return 'Lighthouse' as the first element from the returned array for the ['hello', 'Lighthouse', 'Labs'] case", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+  it("return 'Labs' as the second element from the returned array for the ['hello', 'Lighthouse', 'Labs'] case", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"])[1], "Labs");
+  });
+  it("return ['Lighthouse', 'Labs'] for the ['hello', 'Lighthouse', 'Labs'] case", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse","Labs"]);
+  });
+  it("return ['hello'] for the ['hello'] case", () => {
+    assert.deepEqual(tail(["hello"]), []);
+  });
+  it("return [] for the [] case", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  
+});
 
-//1 - Check the returned array elements by checking its length and elements and compare them to the expected values
-//This allows us to still use assertEqual indirectly to check that the returning array is correct.
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs
 
-//2 - Checking whether the original array has been modified or not.
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-// checking if the original array has been modified after it has been used in the head function.
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-//3 - Check for unique cases such an array with a length of 1 or an empty array (they should both return an empty array)
-//We can check the above statement by ensuring that both returned arrays have a length of 0.
-const emptyInput = tail([]);
-assertEqual(emptyInput.length, 0);
 
-const oneInput = tail(["hello"]);
-assertEqual(oneInput.length, 0);
